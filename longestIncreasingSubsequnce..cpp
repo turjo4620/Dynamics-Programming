438767bdf5822bc3a079dffd0c94c80fa2d4df34
turjo4620 <sarkerturjo2022@gmail.com>
2026-03-19 01:52:47 +0600

    added

--- longestIncreasingSubsequnce..cpp
+++ longestIncreasingSubsequnce..cpp
@@ -2,25 +2,32 @@
 
 using namespace std;
 
-
-int longestIncreasingSubsequence(int index, int prev, vector<int>&vec){
-    if(index == vec.size()){
-        return 0;
-    }
-    vector<vector<int>>dp(vec.size(), vector<int>(-1, vec.size() + 1));
-    if(dp[index][prev+1] != -1)return dp[index][prev + 1];
-
-    int not_take = longestIncreasingSubsequence(index + 1, prev, vec);
-    int take;
-    if(prev != -1 || vec[index] > vec[prev]){
-        take = 1 + longestIncreasingSubsequence(index + 1, index, vec);
-    }
-    int len = max(not_take, take);
-    return dp[index][prev + 1] = len;
-}
-
-
 int main(){
     
     return 0;
 }
+
+
+class Solution {
+public:
+    int helper(int index, int prev, vector<int>&nums, vector<vector<int>>&dp){
+        int n = nums.size();
+        if(index == n){
+            return 0;
+        }
+        if(dp[index][prev + 1] != -1) return dp[index][prev + 1];
+        int not_take = helper(index + 1, prev, nums, dp);
+        int take;
+        if(prev == -1 || nums[index] > nums[prev]){
+            take = 1 + helper(index + 1, index, nums, dp);
+        }
+        int len = max(take, not_take);
+        return dp[index][prev + 1] = len;
+
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>dp(n, vector<int>(n+1, -1));
+        return helper(0, -1, nums, dp);
+    }
+};
